a398b33df8f70864b411a8c0c630f20bb9abf4ed
xliu614 <vivianne.liu@gmail.com>
2024-01-11 16:41:33 -0800

    Add Custom Validator for Size of  Shirt

--- WebAPI_MinimalAPI/Models/Shirt.cs
+++ WebAPI_MinimalAPI/Models/Shirt.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using WebAPI_MinimalAPI.Models.Validations;
 
 namespace WebAPI_MinimalAPI.Models
 {
@@ -9,6 +10,8 @@ namespace WebAPI_MinimalAPI.Models
         public string? Brand { get; set; }
         [Required]
         public string? Color { get; set; }
+        [Range(1, 20)]
+        [Shirt_EnsureCorrectSize]
         public int? Size { get; set; }
         [Required]
         public string? Gender { get; set; }
