ef40650864466a9d79c8b322ea36b4c415f2e65d
xliu614 <vivianne.liu@gmail.com>
2024-01-22 12:14:44 -0800

    Removed in memory list of Shirts

--- WebAPI_MinimalAPI/Models/Repositories/ShirtRepository.cs
+++ WebAPI_MinimalAPI/Models/Repositories/ShirtRepository.cs
@@ -10,14 +10,7 @@ namespace WebAPI_MinimalAPI.Models.Repositories
         {
             this._context = dbContext;   
         }
-        private static readonly List<Shirt> shirts = new List<Shirt>()
-        {
-           new Shirt {ShirtId=1, Brand = "V Brand", Color ="Yellow", Gender="women", Price = 30, Size=6 },
-           new Shirt {ShirtId=2, Brand = "V Brand", Color ="Blue", Gender="women", Price = 30, Size=7 },
-           new Shirt {ShirtId=3, Brand = "W Brand", Color ="Purple", Gender="women", Price = 30, Size=7 },
-           new Shirt {ShirtId=4, Brand = "W Brand", Color ="Black", Gender="men", Price = 30, Size=10 },
-           new Shirt {ShirtId=5, Brand = "Y Brand", Color ="White", Gender="women", Price = 10, Size = 6 }
-        };
+
 
         public List<Shirt> GetShirts() {
             return _context.Shirts.ToList();
