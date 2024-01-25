eb8b099baed627de0755d046a90161949f171c2e
xliu614 <vivianne.liu@gmail.com>
2024-01-25 14:54:41 -0800

    Add Authority and do the registor for authentication

---
+++ WebAPI_MinimalAPI/Authority/AppRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace WebAPI_MinimalAPI.Authority
+{
+	public static class AppRepository
+	{
+		private static List<Application> _application = new List<Application>()
+		{
+			new Application {
+				ApplicationId = 1,
+				ApplicationName = "MVCWebApp",
+				ClientId = "3543C19F-875C-4F3D-8351-E2EC3221625B",
+				Secret = "919B8371-1133-44FD-B145-3F1B1A5759BD"
+			}
+		};
+
+		public static bool Authenticate(string clientId, string secret) {
+			return _application.Any(a => a.ClientId == clientId && a.Secret == secret);
+		}
+
+		public static Application? GetApplicationByClientId(string clientId)
+		{
+			return _application.FirstOrDefault(a => a.ClientId == clientId);
+		}
+
+
+	}
+}
---
+++ WebAPI_MinimalAPI/Authority/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI_MinimalAPI.Authority
+{
+	public class Application
+	{
+        public int ApplicationId { get; set; }
+		public string? ApplicationName { get; set; }
+        public string? ClientId { get; set; }
+        public string? Secret { get; set; }
+    }
+}
